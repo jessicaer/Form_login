8da6ee44b54d42eabea3148eb5c6440a18a6253a
Jessica Elizabeth R <52309435+jessicaer@users.noreply.github.com>
2021-04-01 20:48:40 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM LOGIN</title>
+</head>
+<body>
+	<form action="login.php" method="POST">
+		<h2>LOGIN</h2>
+		<label>Username</label>
+		<input type="text" name="uname" placeholder="User Name">
+
+		<label>Password</label>
+		<input type="text" name="password" placeholder="Password">
+
+		<button type="submit">Login</button>
+	</form>
+
+</body>
+</html>
